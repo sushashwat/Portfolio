b2ec4a7a774685d34eb67eec50570c1c7c9162c1
Shashwat Gupta <guptashashwat155@gmail.com>
2026-07-08 22:42:15 +0530

    feat: added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p>Built by Shashwat Gupta —{" "}
+                <span>
+                    // designed, coded & deployed like everything else here.
+                </span>
+            </p>
+        </footer>
+    )
+}
